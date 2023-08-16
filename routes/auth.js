8f6c400d952a63e77df538f9decd46348ec6cf37
janidu98 <janinduchathushan@gmail.com>
2023-08-16 12:49:01 +0530

    commit2

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,27 @@
 const User = require('../models/User');
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
 
-router.get('/register', async (req, res) => {
-    const user = await new User({
-        username: "john",
-        email: "john@gmail.com",
-        password: "123456"
-    })
+router.post('/register', async (req, res) => {
+    
+    try{
+        //generate new password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-    await user.save();
-    res.send('ok');
+        //create new user
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword,
+        })
+
+        //save user and respond
+        const user = await newUser.save();
+        res.status(200).json(user);
+    } catch(err){
+        console.log(err);
+    }
 })
 
 module.exports = router; 
